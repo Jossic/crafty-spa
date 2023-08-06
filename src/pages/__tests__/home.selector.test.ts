6d058ffa55f3d85ab101fe5028860b4b2085ee29
Jossic <jossic.lapierre@gmail.com>
2023-08-06 22:54:59 +0200

    feat: handle timeline with zero, no and 1 messages

--- src/pages/__tests__/home.selector.test.ts
+++ src/pages/__tests__/home.selector.test.ts
@@ -3,37 +3,86 @@ import { describe, expect, it } from 'vitest';
 import { selectHomeSelector } from '@/pages/home.selector';
 
 describe('view model / selector', function() {
-  it("Example: There is no timeline in the store", () => {
+  it('Example: There is no timeline in the store', () => {
     const store = createTestStore();
 
     const homeSelector = selectHomeSelector(store.getState());
 
     expect(homeSelector).toEqual({
-      timeline: null
-    })
-  })
+      timeline: {
+        type: 'TIMELINE_NOT_FOUND',
+      },
+    });
+  });
 
-  it("Example: There is no messages in the timeline", () => {
+  it('Example: There is no messages in the timeline', () => {
     const store = createTestStore({}, {
       timelines: {
-        ids: ["alice-timeline-1"],
+        ids: ['alice-timeline-1'],
         entities: {
-          "alice-timeline-1": {
-            id: "alice-timeline-1",
-            user: "Alice",
-            messages: ["msg"]
+          'alice-timeline-1': {
+            id: 'alice-timeline-1',
+            user: 'Alice',
+            messages: [],
 
-          }
-        }
-      }
+          },
+        },
+      },
     });
 
     const homeSelector = selectHomeSelector(store.getState());
 
     expect(homeSelector).toEqual({
       timeline: {
-        messages: []
-      }
-    })
-  })
+        messages: {
+          type: 'EMPTY_TIMELINE',
+          content: 'There is no messages in this timeline',
+        },
+      },
+    });
+  });
+
+  it('Example: There is one message in the timeline', () => {
+    const store = createTestStore({}, {
+      timelines: {
+        ids: ['alice-timeline-1'],
+        entities: {
+          'alice-timeline-1': {
+            id: 'alice-timeline-1',
+            user: 'Alice',
+            messages: ['msg-1'],
+
+          },
+        },
+      },
+      messages: {
+        ids: ['msg-1'],
+        entities: {
+          'msg-1': {
+            id: 'msg-1',
+            author: 'Alice',
+            text: 'Hello world!',
+            publishedAt: '2021-10-10T10:00:00Z',
+          },
+        },
+      },
+    });
+
+    const homeSelector = selectHomeSelector(store.getState());
+
+    expect(homeSelector).toEqual({
+      timeline: {
+        type: 'TIMELINE_MESSAGES',
+        messages: [
+          {
+            id: 'msg-1',
+            author: 'Alice',
+            text: 'Hello world!',
+            publishedAt: '2021-10-10T10:00:00Z',
+          },
+        ],
+
+      },
+    });
+  });
 });
--- src/pages/home.selector.ts
+++ src/pages/home.selector.ts
@@ -1,13 +1,35 @@
 import { RootState } from '@/lib/create-store';
 import { selectTimeline } from '@/lib/timelines/slices/timelines.slice';
+import { selectMessage } from '@/lib/timelines/slices/messages.slice';
 
 export const selectHomeSelector = (rootState: RootState) => {
 
-  const timeline = selectTimeline( "alice-timeline-1", rootState);
+  const timeline = selectTimeline('alice-timeline-1', rootState);
 
-  if (!timeline) return { timeline: null };
+  if (!timeline) return {
+    timeline: {
+      type: 'TIMELINE_NOT_FOUND',
+    },
+  };
 
-  return { timeline: {
-    messages: []
-    } }
+  if (timeline.messages.length === 0)
+    return {
+      timeline: {
+        messages: {
+          type: 'EMPTY_TIMELINE',
+          content: 'There is no messages in this timeline',
+        },
+      },
+    };
+
+  const message = selectMessage(timeline.messages[0], rootState);
+
+  return {
+    timeline: {
+      type: 'TIMELINE_MESSAGES',
+      messages: [
+        message
+      ],
+    }
+  }
 };
